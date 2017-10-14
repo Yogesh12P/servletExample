d5f8d634dd6aa0341d968214473bd078fd487512
Yogesh12P <patil1yogesh21@gmail.com>
2017-10-14 12:17:35 +0530

    Add files via upload
    after adding filter..,

--- loginForm/src/com/loginForm/filters/loginFilter.java
+++ loginForm/src/com/loginForm/filters/loginFilter.java
@@ -1,27 +1,42 @@
 package com.loginForm.filters;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector.Matcher;
 
 /**
  * Servlet Filter implementation class loginFilter
  */
 @WebFilter("/loginFilter")
 public class loginFilter implements Filter {
-
+	
     /**
      * Default constructor. 
      */
     public loginFilter() {
         // TODO Auto-generated constructor stub
+    	
+    
     }
 
+    
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
 	/**
 	 * @see Filter#destroy()
 	 */
@@ -37,7 +52,22 @@ public class loginFilter implements Filter {
 		// place your code here
 
 		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		//chain.doFilter(request, response);
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		//String password = request.getParameter("");
+		System.out.println("filter called");
+		if(isValidEmailAddress(username)) {
+			chain.doFilter(request, response);
+			System.out.println("Email is valid");
+		}else
+		{
+			out.print("username or password error!");  
+		    RequestDispatcher rd=request.getRequestDispatcher("signUp.html");  
+		    rd.include(request, response);  
+		}
+		
 	}
 
 	/**
